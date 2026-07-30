4c9110675e811cd03b546fa7e590ccf5b4aa6578
Luke Hill <lukehill_uk@hotmail.com>
2026-07-30 16:39:14 +0100

    Fix performance cops

--- test/scenarios/no_assertion_library.rb
+++ test/scenarios/no_assertion_library.rb
@@ -5,7 +5,7 @@ require 'multi_test'
 begin
   MultiTest.extend_with_best_assertion_library(self)
 rescue NoMethodError => e
-  raise 'no assertion library detected' if e.message =~ /extend_world/
+  raise 'no assertion library detected' if e.message.include?('extend_world')
 
   raise e
 end
