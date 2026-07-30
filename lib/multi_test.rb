589def7b3589c6ce1c0e1f65f26861a714fdaab5
Luke Hill <lukehill_uk@hotmail.com>
2026-07-30 10:57:23 +0100

    AF a bunch of remaining style cop offenses

--- lib/multi_test.rb
+++ lib/multi_test.rb
@@ -6,25 +6,24 @@ module MultiTest
   def self.disable_autorun
     Test::Unit::Runner.module_eval('@@stop_auto_run = true') if defined?(Test::Unit::Runner)
 
-    if defined?(Minitest)
-      Minitest.instance_eval do
-        def run(*)
-          # propagate the exit code from cucumber or another runner
-          case $ERROR_INFO
-          when SystemExit
-            $ERROR_INFO.status
-          else
-            true
-          end
-        end
-      end
+    return unless defined?(Minitest)
 
-      if defined?(Minitest::Unit)
-        Minitest::Unit.class_eval do
-          def run(*)
-          end
+    Minitest.instance_eval do
+      def run(*)
+        # propagate the exit code from cucumber or another runner
+        case $ERROR_INFO
+        when SystemExit
+          $ERROR_INFO.status
+        else
+          true
         end
       end
+    end
+
+    return unless defined?(Minitest::Unit)
+
+    Minitest::Unit.class_eval do
+      def run(*); end
     end
   end
 
