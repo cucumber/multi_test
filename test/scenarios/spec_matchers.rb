93cfc034d41caaa42b5534308de500a8eb58d219
Luke Hill <lukehill_uk@hotmail.com>
2026-07-30 13:13:03 +0100

    Ensure activesupport-8 runs with minitest-5 for now

--- test/scenarios/spec_matchers.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'multi_test'
-
-# Now we create the world
-MultiTest.extend_with_best_assertion_library(self)
-
-# Now we execute a scenario and assert something
-1.should == 1
