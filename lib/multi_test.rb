e72a2cfef9dd35d7b21239f2d67cfdadffc62e65
Luke Hill <lukehill_uk@hotmail.com>
2026-07-30 11:37:39 +0100

    Add notes for stdlib ruby removal of gem

--- lib/multi_test.rb
+++ lib/multi_test.rb
@@ -4,39 +4,13 @@ require 'English'
 require 'multi_test/assertion_library'
 module MultiTest
   class << self
+    # TODO: Remove in v3 (Targeting minispec and external gem removals first)
     def disable_autorun
       Test::Unit::Runner.module_eval('@@stop_auto_run = true', __FILE__, __LINE__) if defined?(Test::Unit::Runner)
-      disable_minitest_autorun
-      disable_minitest_unit_autorun
     end
 
     def extend_with_best_assertion_library(object)
       AssertionLibrary.detect_best.extend_world(object)
-    end
-
-    private
-
-    def disable_minitest_autorun
-      return unless defined?(Minitest)
-
-      Minitest.instance_eval do
-        def run(*)
-          # propagate the exit code from cucumber or another runner
-          if $ERROR_INFO.is_a?(SystemExit)
-            $ERROR_INFO.status
-          else
-            true
-          end
-        end
-      end
-    end
-
-    def disable_minitest_unit_autorun
-      return unless defined?(Minitest::Unit)
-
-      Minitest::Unit.class_eval do
-        def run(*); end
-      end
     end
   end
 end
